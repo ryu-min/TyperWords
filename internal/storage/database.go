53b64dc9dddf89fe3d864b1b125914e91f355840
ryu-min <SergeyR6@yandex.ru>
2023-04-02 20:02:20 +0300

    core refactoring

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -11,7 +11,6 @@ import (
 	mapset "github.com/deckarep/golang-set/v2"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/spf13/viper"
 )
 
 //go:embed words/*.txt
@@ -45,37 +44,48 @@ func New() *Database {
 	const file string = "words.db"
 	handler, err := sql.Open("sqlite3", file)
 	if err != nil {
-		log.Print(err)
-		panic("can't connect to database")
+		log.Print("Can't connect to database")
+		panic(err)
 	}
 
 	err = handler.Ping()
 	if err != nil {
-		log.Print(err)
-		panic("can't ping to database")
+		log.Print("Can't ping to database")
+		panic(err)
 	}
 	database := &Database{
 		db: handler,
 	}
-	database.configing()
+	err = database.configing()
+	if err != nil {
+		log.Print("Configuring error")
+		panic(err)
+	}
 	return database
 }
 
-func (d *Database) configing() {
-	reset := viper.Get("reset").(bool)
-	if reset {
-		fmt.Println("reset all data...")
-		err := d.resetDb()
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		fmt.Println("check all data...")
-		err := d.checkDb()
-		if err != nil {
-			panic(err)
+func (d *Database) configing() error {
+	fmt.Println("check all data...")
+	words, err := getWords(&wordsFS)
+	if err != nil {
+		return err
+	}
+	existingTable, err := d.requestAllTablesName()
+	if err != nil {
+		return err
+	}
+	for wordsType, content := range words {
+		if existingTable.Contains(wordsType) {
+			fmt.Printf("data %s already stored \n", wordsType)
+		} else {
+			fmt.Printf("store %s ... \n", wordsType)
+			err := d.createTableWithContent(wordsType, strings.Fields(content))
+			if err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
 func (d *Database) GetWordTypes() ([]string, error) {
@@ -84,26 +94,6 @@ func (d *Database) GetWordTypes() ([]string, error) {
 		return nil, err
 	}
 	return typeSet.ToSlice(), nil
-}
-
-func (d *Database) resetDb() error {
-	words, err := getWords(&wordsFS)
-	if err != nil {
-		return err
-	}
-	err = d.removeAllTables()
-	if err != nil {
-		return err
-	}
-	for wordsType, content := range words {
-		fmt.Printf("add data to table %s ... \n", wordsType)
-		err = d.createTableWithContent(wordsType, strings.Fields(content))
-		if err != nil {
-			return err
-		}
-	}
-	fmt.Println(len(words))
-	return nil
 }
 
 func (d *Database) checkDb() error {
@@ -131,7 +121,6 @@ func (d *Database) checkDb() error {
 
 func (d *Database) requestAllTablesName() (mapset.Set[string], error) {
 	rows, err := d.db.Query("SELECT name FROM sqlite_master WHERE type='table'")
-
 	if err != nil {
 		return nil, err
 	}
@@ -145,13 +134,11 @@ func (d *Database) requestAllTablesName() (mapset.Set[string], error) {
 		}
 		result.Add(tableName)
 	}
-	fmt.Println(result)
 	return result, nil
 }
 
 func (d *Database) createTableWithContent(name string, content []string) error {
 	queryString := fmt.Sprintf("CREATE TABLE %s ( word TEXT PRIMARY KEY);", name)
-	fmt.Println(queryString)
 	statement, err := d.db.Prepare(queryString)
 	if err != nil {
 		return err
@@ -160,43 +147,18 @@ func (d *Database) createTableWithContent(name string, content []string) error {
 	if err != nil {
 		return err
 	}
-
 	for _, word := range content {
 		queryString := fmt.Sprintf("INSERT INTO %s (word) VALUES (\"%s\")", name, word)
-
-		fmt.Println(queryString)
 		statement, err := d.db.Prepare(queryString)
 		if err != nil {
+			fmt.Printf("error on adding word %s", word)
 			return err
 		}
 		_, err = statement.Exec()
 		if err != nil {
+			fmt.Printf("error on adding word %s", word)
 			return err
 		}
-
-		// fmt.Println(queryString)
-		// intertResult, err := d.db.Query(queryString)
-		// if intertResult != nil {
-		// 	intertResult.Close()
-		// }
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	panic(err)
-		// }
 	}
-	return nil
-}
-
-func (d *Database) removeAllTables() error {
-	dropResult, err := d.db.Query("DROP SCHEMA public CASCADE")
-	if err != nil {
-		return err
-	}
-	defer dropResult.Close()
-	createResult, err := d.db.Query("CREATE SCHEMA public")
-	if err != nil {
-		return err
-	}
-	defer createResult.Close()
 	return nil
 }
